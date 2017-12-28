51a63ac4587e4d9b9cf040c51337180715fc9a7b
Linda <s4_kalasn_l@venta.lv>
2017-12-28 22:19:34 +0200

    Changed mapped values to point to related database stuf

--- src/main/java/com/vea/zoo/Zoo/dao/SoldTicketDBAccess.java
+++ src/main/java/com/vea/zoo/Zoo/dao/SoldTicketDBAccess.java
@@ -4,60 +4,61 @@ import com.vea.zoo.Zoo.SoldTickets;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class SoldTicketDBAccess implements SoldTicketDao{
-    //TODO Overridden functions
-    @Override
-    public <S extends SoldTickets> S save(S s) {
-        return null;
-    }
-
-    @Override
-    public <S extends SoldTickets> Iterable<S> save(Iterable<S> iterable) {
-        return null;
-    }
-
-    @Override
-    public SoldTickets findOne(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public boolean exists(Long aLong) {
-        return false;
-    }
-
-    @Override
-    public Iterable<SoldTickets> findAll() {
-        return null;
-    }
-
-    @Override
-    public Iterable<SoldTickets> findAll(Iterable<Long> iterable) {
-        return null;
-    }
-
-    @Override
-    public long count() {
-        return 0;
-    }
-
-    @Override
-    public void delete(Long aLong) {
-
-    }
-
-    @Override
-    public void delete(SoldTickets soldTickets) {
-
-    }
-
-    @Override
-    public void delete(Iterable<? extends SoldTickets> iterable) {
-
-    }
-
-    @Override
-    public void deleteAll() {
-
-    }
-}
+public class SoldTicketDBAccess{}
+////implements SoldTicketDao{
+//    //TODO Overridden functions
+//    @Override
+//    public <S extends SoldTickets> S save(S s) {
+//        return null;
+//    }
+//
+//    @Override
+//    public <S extends SoldTickets> Iterable<S> save(Iterable<S> iterable) {
+//        return null;
+//    }
+//
+//    @Override
+//    public SoldTickets findOne(Long aLong) {
+//        return null;
+//    }
+//
+//    @Override
+//    public boolean exists(Long aLong) {
+//        return false;
+//    }
+//
+//    @Override
+//    public Iterable<SoldTickets> findAll() {
+//        return null;
+//    }
+//
+//    @Override
+//    public Iterable<SoldTickets> findAll(Iterable<Long> iterable) {
+//        return null;
+//    }
+//
+//    @Override
+//    public long count() {
+//        return 0;
+//    }
+//
+//    @Override
+//    public void delete(Long aLong) {
+//
+//    }
+//
+//    @Override
+//    public void delete(SoldTickets soldTickets) {
+//
+//    }
+//
+//    @Override
+//    public void delete(Iterable<? extends SoldTickets> iterable) {
+//
+//    }
+//
+//    @Override
+//    public void deleteAll() {
+//
+//    }
+//}
--- src/main/java/com/vea/zoo/Zoo/dao/VoucherDBAccess.java
+++ src/main/java/com/vea/zoo/Zoo/dao/VoucherDBAccess.java
@@ -7,63 +7,64 @@ import javax.swing.text.html.HTMLDocument;
 import java.util.Iterator;
 
 @Repository
-public class VoucherDBAccess implements VoucherDao {
-    //TODO Overridden functions
-    @Override
-    public <S extends Voucher> S save(S s) {
-        return null;
-    }
-
-    @Override
-    public <S extends Voucher> Iterable<S> save(Iterable<S> iterable) {
-        return null;
-    }
-
-    @Override
-    public Voucher findOne(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public boolean exists(Long aLong) {
-        return false;
-    }
-
-    @Override
-    public Iterable<Voucher> findAll() {
-        return null;
-    }
-
-    @Override
-    public Iterable<Voucher> findAll(Iterable<Long> iterable) {
-        return null;
-    }
-
-    @Override
-    public long count() {
-        return 0;
-    }
-
-    @Override
-    public void delete(Long aLong) {
-        //No Deletion currently
-    }
-
-    @Override
-    public void delete(Voucher voucher) {
-        //No Deletion currently
-    }
-
-    @Override
-    public void delete(Iterable<? extends Voucher> iterable) {
-        //No Deletion currently
-    }
-
-    @Override
-    public void deleteAll() {
-        //No Deletion currently
-    }
-    public VoucherDBAccess(){
-
-    }
-}
+public class VoucherDBAccess {}
+//        implements VoucherDao {
+//    //TODO Overridden functions
+//    @Override
+//    public <S extends Voucher> S save(S s) {
+//        return null;
+//    }
+//
+//    @Override
+//    public <S extends Voucher> Iterable<S> save(Iterable<S> iterable) {
+//        return null;
+//    }
+//
+//    @Override
+//    public Voucher findOne(Long aLong) {
+//        return null;
+//    }
+//
+//    @Override
+//    public boolean exists(Long aLong) {
+//        return false;
+//    }
+//
+//    @Override
+//    public Iterable<Voucher> findAll() {
+//        return null;
+//    }
+//
+//    @Override
+//    public Iterable<Voucher> findAll(Iterable<Long> iterable) {
+//        return null;
+//    }
+//
+//    @Override
+//    public long count() {
+//        return 0;
+//    }
+//
+//    @Override
+//    public void delete(Long aLong) {
+//        //No Deletion currently
+//    }
+//
+//    @Override
+//    public void delete(Voucher voucher) {
+//        //No Deletion currently
+//    }
+//
+//    @Override
+//    public void delete(Iterable<? extends Voucher> iterable) {
+//        //No Deletion currently
+//    }
+//
+//    @Override
+//    public void deleteAll() {
+//        //No Deletion currently
+//    }
+//    public VoucherDBAccess(){
+//
+//    }
+//}
