703820d3c2ded65e53d72c52dc9c982e1140f68c
Linda <s4_kalasn_l@venta.lv>
2017-12-28 21:12:29 +0200

    changed anotation to controller

--- src/main/java/com/vea/zoo/Zoo/contoller/TicketController.java
+++ src/main/java/com/vea/zoo/Zoo/contoller/TicketController.java
@@ -12,17 +12,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.LinkedList;
 import java.util.List;
-@Controller("/ticketStatistics")
-@Transactional
-@EnableWebMvc
+@Controller
 public class TicketController {
     @Autowired
     private TicketDao ticketDao;
