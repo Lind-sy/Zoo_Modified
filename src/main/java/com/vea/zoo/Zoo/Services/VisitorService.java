ff41731d7870151556ae36e19ffd1cc74308cb6e
Linda <s4_kalasn_l@venta.lv>
2018-01-11 12:56:14 +0200

    Added logic to delete all vouchers connected to visitor when visitor is deleted

--- src/main/java/com/vea/zoo/Zoo/Services/VisitorService.java
+++ src/main/java/com/vea/zoo/Zoo/Services/VisitorService.java
@@ -4,6 +4,7 @@ import com.vea.zoo.Zoo.Model.HumanCatorgorys;
 import com.vea.zoo.Zoo.Model.Visitor;
 import com.vea.zoo.Zoo.dao.HumanCategoryDao;
 import com.vea.zoo.Zoo.dao.SoldTicketDao;
+import com.vea.zoo.Zoo.dao.SoldVoucherDao;
 import com.vea.zoo.Zoo.dao.VisitorDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,14 +17,17 @@ public class VisitorService {
     private VisitorDao visitorDao;
     private HumanCategoryDao humanCategoryDao;
     private SoldTicketDao soldTicketDao;
+    private SoldVoucherDao soldVoucherDao;
 
     @Autowired
     public VisitorService( final VisitorDao visitorDao,
                            final HumanCategoryDao humanCategoryDao,
-                           final SoldTicketDao soldTicketDao) {
+                           final SoldTicketDao soldTicketDao,
+                           final SoldVoucherDao soldVoucherDao) {
         this.visitorDao = visitorDao;
         this.humanCategoryDao = humanCategoryDao;
         this.soldTicketDao = soldTicketDao;
+        this.soldVoucherDao = soldVoucherDao;
     }
 
     public List<Visitor> getAllVisitors(){
@@ -42,6 +46,7 @@ public class VisitorService {
 
     public void deleteVisitor(Long visitorId) {
         soldTicketDao.delete(soldTicketDao.findByVisitorId(visitorId));
+        soldVoucherDao.delete(soldVoucherDao.findByVisitorId(visitorId));
         visitorDao.delete(visitorId);
     }
 
--- src/main/java/com/vea/zoo/Zoo/dao/SoldVoucherDao.java
+++ src/main/java/com/vea/zoo/Zoo/dao/SoldVoucherDao.java
@@ -1,9 +1,16 @@
 package com.vea.zoo.Zoo.dao;
 
 import com.vea.zoo.Zoo.Model.SoldVouchers;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SoldVoucherDao  extends CrudRepository<SoldVouchers, Long> {
+
+    @Query("select s from SoldVouchers s where s.zooVisitorOwner.id =:visitorId")
+    List<SoldVouchers> findByVisitorId(@Param("visitorId") Long visitorId);
 }
